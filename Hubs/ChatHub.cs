01740b1c34fa2d08547372d5a9077e697ea9437d
Rakib Ahasan <bd.rakibahasan@gmail.com>
2025-06-21 17:11:14 +0600

    2nd commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task JoinGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+        }
+
+        public async Task SendMessageToGroup(string groupName, string user, string message)
+        {
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);
+        }
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+
+
+    }
+}
